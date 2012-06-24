003893c5114dc85b6181d24fb21861ece832e99c
Kyr Dunenkoff <dunenkoff@me.com>
2012-06-24 14:21:52 +0400

    Initial Commit

---
+++ FlipClock/KDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KDAppDelegate.h
+//  FlipClock
+//
+//  Created by Kyr Dunenkoff on 6/24/12.
+//  Copyright (c) 2012 Solid Applications LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlipClock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlipClock
+//
+//  Created by Kyr Dunenkoff on 6/24/12.
+//  Copyright (c) 2012 Solid Applications LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KDAppDelegate class]));
+    }
+}
